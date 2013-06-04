8c9e248cf3a499e2efc29a944e4e5abe7fcde254
eldie1984 <eldie1984@gmail.com>
2013-06-04 02:13:37 -0300

    Se realiza la clase funciones con los datos necesarios para poblar los combobox y se genera la funcion de insert de recorridos

--- FrbaBus/Abm Recorrido/RecorridoCrear.cs
+++ FrbaBus/Abm Recorrido/RecorridoCrear.cs
@@ -29,23 +29,52 @@ namespace FrbaBus.Abm_Recorrido
         }
         public void llenacombobox()
         {
-            
-            this.cadenaConexion = (@"Data Source=PC_PRUEBA\SQLSERVER2008;Initial Catalog =GD1C2013; integrated security =true;User Id=gd;Password=gd2013;");
-            this.cnn = new SqlConnection(cadenaConexion);
-            this.sql = string.Format(@"select * from ciudades");
-            DataSet ds = new DataSet();
-            DataSet ds1 = new DataSet();
-            //indicamos la consulta en SQL
-            SqlDataAdapter da = new SqlDataAdapter("select ciud_nombre from transportados.ciudad", this.cnn);
-            //se indica el nombre de la tabla
-            da.Fill(ds, "Ciudad");
-            da.Fill(ds1, "Ciudad");
-            comboBox1.DataSource = ds.Tables[0].DefaultView;
+            funciones dataCiudad = new funciones();
+            /*SqlDataAdapter da = dataCiudad.llenaComboboxCiudad();
+            DataSet ds_origen = new DataSet();
+            DataSet ds_destino = new DataSet();*/
+            DataSet ds_origen = dataCiudad.llenaComboboxCiudad();
+            DataSet ds_destino = dataCiudad.llenaComboboxCiudad();
+            DataSet ds_tipo = dataCiudad.llenaComboboxTipo();
+            //da.Fill(ds_origen, "Ciudad");
+            comboBox1.DataSource = ds_origen.Tables[0].DefaultView;
             //se especifica el campo de la tabla
-            comboBox1.ValueMember = "ciud_nombre";
-            comboBox2.DataSource = ds1.Tables[0].DefaultView;
+            comboBox1.DisplayMember = "ciud_nombre";
+            comboBox1.ValueMember = "ciud_id";
+            //da.Fill(ds_destino, "Ciudad");
+            comboBox2.DataSource = ds_destino.Tables[0].DefaultView;
             //se especifica el campo de la tabla
-            comboBox2.ValueMember = "ciud_nombre";
+            comboBox2.DisplayMember = "ciud_nombre";
+            comboBox2.ValueMember = "ciud_id";
+            comboBox3.DataSource = ds_tipo.Tables[0].DefaultView;
+            //se especifica el campo de la tabla
+            comboBox3.DisplayMember = "tipo_nombre";
+            comboBox3.ValueMember = "tipo_id";
+            //int value = ((KeyValuePair<string, int>)comboBox1.SelectedItem).Value;
+            //MessageBox.Show(value.ToString());
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool result;
+            funciones dataCiudad = new funciones();
+            result=dataCiudad.insertarRecorrido(Convert.ToInt32(comboBox1.SelectedValue), Convert.ToInt32(comboBox2.SelectedValue), Convert.ToInt32(comboBox3.SelectedValue), Convert.ToInt32(this.textBox1.Text), Convert.ToInt32(this.textBox2.Text));
+            if (result == true)
+            {
+                MessageBox.Show("Iupppi");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Error");
+                this.Close();
+            }
         }
 
     }
---
+++ FrbaBus/funciones.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+namespace FrbaBus
+{
+    class funciones:conexion
+    {
+        public DataSet llenaComboboxCiudad()
+        {
+            this.sql = string.Format(@"select ciud_id,ciud_nombre from transportados.ciudad");
+            DataSet ds = new DataSet();
+            //indicamos la consulta en SQL
+            SqlDataAdapter da = new SqlDataAdapter(this.sql, this.cnn);
+            //return da;
+            da.Fill(ds, "Ciudad");
+            return ds;
+        }
+        public DataSet llenaComboboxTipo()
+        {
+            this.sql = string.Format(@"select tipo_id,tipo_nombre from transportados.tipo_servicio");
+            DataSet ds = new DataSet();
+            //indicamos la consulta en SQL
+            SqlDataAdapter da = new SqlDataAdapter(this.sql, this.cnn);
+            //return da;
+            da.Fill(ds, "tipo_servicio");
+            return ds;
+        }
+        public bool insertarRecorrido(int ciudOrigen, int ciudDestino,int tipoServ,int basePasaje,int baseEncomienda)
+        {
+            bool Resultado = false;
+            int result = 0;
+            //this.sql = string.Format(@"select tipo_id,tipo_nombre from transportados.tipo_servicio");
+            this.sql = string.Format(@"INSERT INTO [GD1C2013].[transportados].[recorrido](
+                                            [reco_id],
+                                            [reco_id_ciudad_origen],
+                                            [reco_id_ciudad_destino],
+                                            [reco_tipo_id],
+                                            [reco_precio_base],
+                                            [reco_precio_encomienda],
+                                            [reco_creado],
+                                            [reco_modificado])
+                                            values
+                                            ('ajbfkasafbksdf',{0},{1},{2},{3},{4},SYSDATETIME(),SYSDATETIME())", ciudOrigen,ciudDestino,tipoServ,basePasaje,baseEncomienda);
+            this.comandosSql = new SqlCommand(this.sql, this.cnn);
+            this.cnn.Open();
+            //SqlDataReader Reg = null;
+            //Reg = this.comandosSql.ExecuteReader();
+            //MessageBox.Show(Reg.Read().ToString());
+            result=this.comandosSql.ExecuteNonQuery();
+            if (result>0)
+            {
+                Resultado = true;
+            }
+            else
+            {
+                Resultado = false;
+            }
+            this.cnn.Close();
+            return Resultado;
+        }
+    }
+}
